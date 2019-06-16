34772270190779070f977c2a0bcb90e8ace6594a
Mateo Tibaquirá <nestormateo@gmail.com>
2019-06-16 18:32:22 -0500

    feat(sponsors): provide datasource to the list component

--- src/app/features/sponsors/list/list.component.ts
+++ src/app/features/sponsors/list/list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { SponsorsDatasource } from '../data';
 
 @Component({
   selector: 'app-feat-sponsors-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  providers: [SponsorsDatasource]
 })
 export class SponsorsListComponent implements OnInit {
-  constructor() {}
+  constructor(public source: SponsorsDatasource) {}
 
   ngOnInit() {}
 }
