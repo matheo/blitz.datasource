478d40455569965199bf3091e291ce1cff9151b5
matheo <nestormateo@gmail.com>
2019-06-16 11:07:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# matdatasource
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/matdatasource)
---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ BrowserAnimationsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
