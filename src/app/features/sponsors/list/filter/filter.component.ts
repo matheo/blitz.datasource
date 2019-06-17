15fd1d95d03cf07e28f395e60aec2e1324fc2797
Mateo Tibaquirá <nestormateo@gmail.com>
2019-06-16 20:23:59 -0500

    feat(sponsors): polish filter styles

--- src/app/features/sponsors/list/filter/filter.component.ts
+++ src/app/features/sponsors/list/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { SponsorsDatasource } from '../../data';
 import { MatPaginator } from '@angular/material';
 import { getterPaginator, DataSourceItem } from '@matheo/datasource';
@@ -7,7 +7,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-feat-sponsors-list-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SponsorsListFilterComponent implements OnInit {
   form: FormGroup;
@@ -21,12 +22,12 @@ export class SponsorsListFilterComponent implements OnInit {
 
   ngOnInit() {
     this.tiers = [
-      { value: 0, title: 'All' },
-      { value: 1, title: 'Tier 1' },
-      { value: 2, title: 'Tier 2' },
-      { value: 3, title: 'Tier 3' },
-      { value: 4, title: 'Tier 4' },
-      { value: 5, title: 'Tier 5' }
+      { value: 0, title: 'All Tiers' },
+      { value: 1, title: 'Tier $1' },
+      { value: 2, title: 'Tier $2' },
+      { value: 3, title: 'Tier $3' },
+      { value: 4, title: 'Tier $4' },
+      { value: 5, title: 'Tier $5' }
     ];
 
     this.form = this.builder.group({
